9789a6a6331399ed6e96943ff589b6d9515fcd22
Tobias Keil <tkeil@3-E-11.42heilbronn.de>
2025-06-26 15:55:31 +0200

    Created get_words_from_file to read from files and to split the content into words. It returns a set of words. Created print_diff, which prints the difference into the terminal. Used sys to get the arguments (file1 and file2) from the user

---
+++ compare.py
@@ -0,0 +1,50 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    compare.py                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tkeil <tkeil@student.42.fr>                +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/06/26 14:49:41 by tkeil             #+#    #+#              #
+#    Updated: 2025/06/26 15:43:00 by tkeil            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import re
+import sys
+
+def get_words_from_file(path):
+	try:
+		with open(path, 'r', encoding='utf-8') as file:
+			text = file.read()
+		words = re.split(r'[\n\r\v\t\f ]+', text)
+		return set(filter(None, words))
+		
+	except FileNotFoundError:
+		print(f"Error: File '{path}' not found.")
+		sys.exit(1)
+
+def print_diff(missing):
+	for word in sorted(missing):
+		print(word)
+
+def main():
+	if len(sys.argv) != 3:
+		print("Usage: python compare.py <file1> <file2>")
+		sys.exit(1)
+	words1 = get_words_from_file(sys.argv[1])
+	words2 = get_words_from_file(sys.argv[2])
+	
+	missing1 = words1 - words2
+	missing2 = words2 - words1
+
+	if missing1:
+		print("\033[92mWords in file1 but not in file2:\033[0m")
+		print_diff(missing1)
+		
+	if missing2:
+		print("\n\033[92mWords in file2 but not in file1:\033[0m")
+		print_diff(missing2)
+
+if __name__ == "__main__":
+	main()
